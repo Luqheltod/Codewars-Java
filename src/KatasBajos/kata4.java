5515563daaedb302fe92be42c426d4e0471b7804
45257012 <luqh555@gmail.com>
2020-10-01 12:58:54 +0200

    kata4

---
+++ src/KatasBajos/kata4.java
@@ -0,0 +1,21 @@
+package KatasBajos;
+
+public class kata4 {
+
+	public static void main(String[] args) {
+		/*
+		 * Pasa un boolean a palabra yes or no con un metodo
+		 */
+
+	}
+
+	 public static String boolToWord(boolean b)
+	  {
+       if (b==true) {
+    	   return "yes";
+       }else {
+    	   return "no";
+       }
+
+	  }
+}
